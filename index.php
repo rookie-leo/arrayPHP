7c71bf35ad2669e979e5dd858dbc9acf0c426dfc
Leonardo Paulino <60435208+rookie-leo@users.noreply.github.com>
2022-03-15 23:11:59 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+
+      $valor_array = array("uni9", 2022);
+
+      print_r($valor_array[0]."<br>");
+      
+      echo("Ano - ".$valor_array[1]);
+
+    ?> 
+  </body>
+</html>
